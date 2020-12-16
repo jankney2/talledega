cbc29df33f557457b86b641b944500a580199442
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-12-16 14:52:00 -0700

    working out function

--- talledega.js
+++ talledega.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const {YEET}=process.env
-console.log(YEET)
+require("dotenv").config();
+const { GOOG } = process.env;
+const moment = require("moment");
+//routes or distance matrix api?
 
+//make iterable- can take in multiple days' worth of data and run
 
+//it'll probably be the directions api since that allows for sequential waypoints
 
+const matrix = ({ origin, destinations, forDate }) => {
+  //request to api
+  //save drive time for the day
+};
 
-const routeInfo=({
-    origin, 
-    destinations, 
-})=>{
+const routes = ({ origin, destination, waypoints, forDate }) => {
 
-
-//request to api 
-
-
-
-}
+    //waypoints should be an array of long/lat combinations 
+  let requestUrl=  `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&waypoints=${waypoints}&key=${GOOG}`;
+};
